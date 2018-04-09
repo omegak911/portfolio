a7e2178ac2706f1188b65fbcfca4ec04227ec5e8
Kin Chan <xkinchanx@gmail.com>
2018-04-09 15:52:44 -0700

    [add] client/server entry

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+const port = 3555;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist')));
+
+app.listen( port, () => console.log('server is connected to port ', port));
