a6b9de0cedc55c5e0499055eebc37eaea4847270
Kin Chan <xkinchanx@gmail.com>
2018-04-10 15:55:29 -0700

    [fix] to allow page persistence upon refresh

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist')));
+app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist/index.html')));
 
-app.listen( port, () => console.log('server is connected to port ', port));
+app.listen( port, () => {
+  console.log('server is connected to port ', port);
+});
