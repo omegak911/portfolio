3db2ad60ea3951f980a7e9f1a816013bc4880d0e
Kin Chan <xkinchanx@gmail.com>
2018-04-10 15:53:19 -0700

    [add]: about stuff

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './About.css';
+
+class About extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <div>About</div>
+        
+      </div>
+    )
+  }
+}
+
+export default About;
