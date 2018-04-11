ef694fd290945f015e3c574b7a0e663405a90e91
Kin Chan <xkinchanx@gmail.com>
2018-04-11 08:39:22 -0700

    [add] stars will render solid/empty based on rating prop

--- client/src/components/Tech/Stars.jsx
+++ client/src/components/Tech/Stars.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Stars = () => (
-  <div>
-    &#x2606;
-    &#x2606;
-    &#x2606;
-    &#x2606;
-    &#x2606;
-  </div>
-)
+import './Stars.css';
+
+const Stars = ({ solidStars }) => {
+  const emptyStars = 5 - solidStars;
+  return (
+    <div>
+      {[...Array(solidStars)].map((star, index) => <span className="solidStar">&#x2605;</span>)}
+      {[...Array(emptyStars)].map((star, index) => <span className="emptyStar">&#x2606;</span>)}
+    </div>
+  )
+}
 export default Stars;
