b6bbff545391d29392def2943576986e9eff9c4e
Kin Chan <xkinchanx@gmail.com>
2018-06-08 21:57:06 -0700

    [add] successfully implemented individual tech rendering with animation

--- client/src/components/Tech/Tech.jsx
+++ client/src/components/Tech/Tech.jsx
@@ -5,100 +5,117 @@ import Stars from './Stars';
 
 import './Tech.css';
 
-export default class Tech extends Component {
+class Tech extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      FrontEnd: [
-        { tech: 'Javascript', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'React', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'Socket.IO', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'HTML5', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'CSS3', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'React Router', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'React Redux', solid: [1,2,3,4], empty: [5] },
-        { tech: 'jQuery', solid: [1,2,3,4], empty: [5] },
-        { tech: 'React Native', solid: [1,2,3], empty: [4,5] },
-      ],
-      BackEnd: [
-        { tech: 'Node.js', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'Express', solid: [1,2,3,4,5], empty: [] },
-        { tech: 'MySQL', solid: [1,2,3,4], empty: [5] },
-        { tech: 'PostgreSQL', solid: [1,2,3,4], empty: [5] },
-        { tech: 'MongoDB', solid: [1,2,3,4], empty: [5] },
-        { tech: 'RESTful API', solid: [1,2,3,4], empty: [5] },
-        { tech: 'BCrypt', solid: [1,2,3,4], empty: [5] },
-        { tech: 'Passport.js', solid: [1,2,3], empty: [4,5] },
-      ],
-      Deployment: [
-        { tech: 'AWS', solid: [1], empty: [2,3,4,5] },
-        { tech: 'Nginx', solid: [],empty: [1,2,3,4,5] },
-      ],
-      DevTools: [
-        { tech: 'Git', solid: [1,2,3,4], empty: [5] },
-        { tech: 'NPM', solid: [1,2,3], empty: [4,5] },
-        { tech: 'Webpack', solid: [1,2,3], empty: [4,5] },
-        { tech: 'Babel', solid: [1,2], empty: [3,4,5] },
-        { tech: 'Yarn', solid: [1,2], empty: [3,4,5] },
-      ],
+      Storage: {
+        FrontEnd: [
+          { tech: 'Javascript', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'React', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'Socket.IO', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'HTML5', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'CSS3', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'React Router', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'React Redux', solid: [1,2,3,4], empty: [5] },
+          { tech: 'jQuery', solid: [1,2,3,4], empty: [5] },
+          { tech: 'React Native', solid: [1,2,3], empty: [4,5] },
+        ],
+        BackEnd: [
+          { tech: 'Node.js', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'Express', solid: [1,2,3,4,5], empty: [] },
+          { tech: 'MySQL', solid: [1,2,3,4], empty: [5] },
+          { tech: 'PostgreSQL', solid: [1,2,3,4], empty: [5] },
+          { tech: 'MongoDB', solid: [1,2,3,4], empty: [5] },
+          { tech: 'RESTful API', solid: [1,2,3,4], empty: [5] },
+          { tech: 'BCrypt', solid: [1,2,3,4], empty: [5] },
+          { tech: 'Passport.js', solid: [1,2,3], empty: [4,5] },
+        ],
+        Deployment: [
+          { tech: 'AWS', solid: [1], empty: [2,3,4,5] },
+          { tech: 'Nginx', solid: [],empty: [1,2,3,4,5] },
+        ],
+        DevTools: [
+          { tech: 'Git', solid: [1,2,3,4], empty: [5] },
+          { tech: 'NPM', solid: [1,2,3], empty: [4,5] },
+          { tech: 'Webpack', solid: [1,2,3], empty: [4,5] },
+          { tech: 'Babel', solid: [1,2], empty: [3,4,5] },
+          { tech: 'Yarn', solid: [1,2], empty: [3,4,5] },
+        ],
+      },
+      FrontEnd: [],
+      BackEnd: [],
+      Deployment: [],
+      DevTools: [],
     }
   }
 
-  // stuff = () => {
-  //   this.setState({ stuff: [...this.state.stuff, 1]})
-  // }
+  componentDidMount() {
+    const { Storage } = this.state;
+    let delay1 = 0;
+    let delay2 = 0;
+    let delay3 = 0;
+    let delay4 = 0;
+    
+    for (let i = 0; i < 9; i++) {
+      setTimeout(() => this.setState({ FrontEnd: [...this.state.FrontEnd, Storage.FrontEnd[i]]}), delay1)
+      delay1 += 1000;
+      if (i < 8) {
+        setTimeout(() => { this.setState({ BackEnd: [...this.state.BackEnd, Storage.BackEnd[i]]})
+      
+        console.log(this.state)
+      }, delay2)
+        delay2 += 1000;
+      }
+      if (i < 2) {
+        setTimeout(() => this.setState({ Deployment: [...this.state.Deployment, Storage.Deployment[i]]}), delay3)
+        delay3 += 1000;
+      }
+      if (i < 5) {
+        setTimeout(() => this.setState({ DevTools: [...this.state.DevTools, Storage.DevTools[i]]}), delay4)
+        delay4 += 1000;
+      }
+    }
+  }
 
   render() {
-    const { frontEnd, backEnd, deployment, devTools } = this.state;
+    const { FrontEnd, BackEnd, Deployment, DevTools } = this.state;
 
     return (
       <div id="tech" className="topContainer">
         <div className="midTechContainer">
           <div>
-            {/* <button onClick={this.stuff}>******</button>
-            <ReactCSSTransitionGroup
-              transitionName="techAnimation"
-              transitionAppear={true}
-              transitionAppearTimeout={500}
-              transitionEnter={false}
-              transitionLeave={false}
-              // transitionEnterTimeout={500}
-              // transitionLeaveTimeout={300}
-              >
-              {this.state.stuff.map((item, index) =>
-                <div key={index}>
-                  {item}
-                </div>
-              )}
-            </ReactCSSTransitionGroup> */}
             <h2>Tech Stack</h2>
             <div className="cover">
-              {Object.keys(this.state).map((techType, index) =>
-                <div className="techType" key={index}>
-                  <div className="techTypeTitle">
-                    <h3>{techType}</h3>
-                  </div>
-                  <div className="techStack">
-                    <ReactCSSTransitionGroup
-                          className="techAnimation"
-                         component='div'
-                          transitionName="techAnimation"
-                          transitionAppear={true}
-                          transitionAppearTimeout={1000}
-                          transitionEnter={false}
-                          transitionLeave={false}>
-                      {this.state[techType].map((tech, index) =>
+              {Object.keys(this.state).map((techType, index) => {
+                if (index > 0) {
+                  return <div className="techType" key={index}>
+                    <div className="techTypeTitle">
+                      <h3>{techType}</h3>
+                    </div>
+                    <div className="techStack">
+                      <ReactCSSTransitionGroup
+                        className="techAnimation"
+                        component='div'
+                        transitionName="techAnimation"
+                        transitionAppear={true}
+                        transitionAppearTimeout={1000}
+                        transitionEnter={true}
+                        transitionEnterTimeout={1000}
+                        transitionLeave={false}>
+                        {this.state[techType].map((tech, index) =>
                           <div className="eachTech" key={index}>
-                              <div id="octagon">
-                                {tech.tech}
-                                <Stars solidStars={tech.solid} emptyStars={tech.empty} />
-                              </div>
+                            <div id="octagon">
+                              {tech.tech}
+                              <Stars solidStars={tech.solid} emptyStars={tech.empty} />
+                            </div>
                           </div>
-                      )}
-                    </ReactCSSTransitionGroup>
+                        )}
+                      </ReactCSSTransitionGroup>
+                    </div>
                   </div>
-                </div>
-              )}
+                }
+              })}
             </div>
           </div>
         </div>
@@ -107,3 +124,5 @@ export default class Tech extends Component {
   }
 
 }
+
+export default Tech;
